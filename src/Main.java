634bfd288cf5fecfa912e1993b3e366b85e28dd1
Artur Sadov <Art.S.V.86@yandex.ru>
2022-02-07 00:01:11 +0400

    Changed value

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        float bonus = 0.05F;
-        float cost = 3000F;
+        float bonus = 0.15F;
+        float cost = 4000F;
         float totalBonus = cost * bonus;
         System.out.println(totalBonus);
     }
