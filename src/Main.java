cefd7f1f48230f69492dcefc63be3112f3e41c4a
Artur Sadov <Art.S.V.86@yandex.ru>
2022-02-07 00:17:50 +0400

    Added files

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        float bonus = 0.15F;
-        float cost = 4000F;
+        float bonus = 0.55F;
+        float cost = 7800F;
         float totalBonus = cost * bonus;
         System.out.println(totalBonus);
     }
